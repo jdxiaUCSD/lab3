d9ecc0d3d8c91c3f8d1ae874f79b129e529ef13d
Jayden Xia <goats@Jaydens-Macbook.local>
2024-04-23 09:57:50 -0700

    Debugged 2 reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,16 +3,29 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlace1Element() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
+
+  @Test 
+	public void testReverseInPlaceMultipleElements() {
+    int[] input1 = { 3, 2, 5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5,2,3 }, input1);
+	}
+  
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+  @Test
+  public void testReversedMultipleElements() {
+    int[] input1 = {1,2,3 };
+    assertArrayEquals(new int[]{3,2,1 }, ArrayExamples.reversed(input1));
   }
 }
